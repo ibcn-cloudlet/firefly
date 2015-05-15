1d045f69ef4417bc462501ca0f2e3cf2444ac4b7
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-05-15 16:24:29 +0200

    add example application project for firefly ui

---
+++ be.iminds.iot.firefly.application/src/be/iminds/iot/firefly/application/FireflyApplication.java
@@ -0,0 +1,23 @@
+package be.iminds.iot.firefly.application;
+
+import org.osgi.service.component.annotations.Component;
+
+import osgi.enroute.capabilities.AngularWebResource;
+import osgi.enroute.capabilities.BootstrapWebResource;
+import osgi.enroute.capabilities.ConfigurerExtender;
+import osgi.enroute.capabilities.WebServerExtender;
+import osgi.enroute.rest.api.REST;
+import osgi.enroute.rest.api.RESTRequest;
+
+@AngularWebResource(resource={"angular.js","angular-resource.js", "angular-route.js"}, priority=1000)
+@BootstrapWebResource(resource="css/bootstrap.css")
+@WebServerExtender
+@ConfigurerExtender
+@Component(name="be.iminds.iot.firefly")
+public class FireflyApplication implements REST {
+
+	public String getUpper(RESTRequest rq, String string) {
+		return string.toUpperCase();
+	}
+
+}
