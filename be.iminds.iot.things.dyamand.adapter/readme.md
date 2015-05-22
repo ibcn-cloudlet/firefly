76e7c4d9ca38ee6242bf799446f4e1822076267c
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-05-22 14:23:17 +0200

    add project for DYAMAND adapter

---
+++ be.iminds.iot.things.dyamand.adapter/readme.md
@@ -0,0 +1,15 @@
+# 
+
+${Bundle-Description}
+
+## Example
+
+## Configuration
+
+	Pid: be.iminds.iot.things.dyamand
+	
+	Field					Type				Description
+		
+	
+## References
+
---
+++ be.iminds.iot.things.dyamand.adapter/src/be/iminds/iot/things/dyamand/adapter/DyamandAdapter.java
@@ -0,0 +1,11 @@
+package be.iminds.iot.things.dyamand.adapter;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * 
+ */
+@Component(name = "be.iminds.iot.things.dyamand")
+public class DyamandAdapter {
+
+}
