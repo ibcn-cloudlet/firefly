368341427b0d1bd0b650d910c7eb4ec3abf455ea
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-05-22 15:15:51 +0200

    bridge DyamandAdapter with Dyamand PluginContext

--- be.iminds.iot.things.dyamand.adapter/src/be/iminds/iot/things/dyamand/adapter/DyamandAdapter.java
+++ be.iminds.iot.things.dyamand.adapter/src/be/iminds/iot/things/dyamand/adapter/DyamandAdapter.java
@@ -1,11 +1,40 @@
 package be.iminds.iot.things.dyamand.adapter;
 
+import org.dyamand.event.EventListener;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.event.EventAdmin;
 
 /**
  * 
  */
-@Component(name = "be.iminds.iot.things.dyamand")
-public class DyamandAdapter {
+@Component(name = "be.iminds.iot.things.dyamand", immediate=true)
+public class DyamandAdapter implements EventListener {
 
+	// Hack to have bridge to Dyamand plugin system...
+	static DyamandAdapter instance = null;
+	
+	private EventAdmin ea;
+	
+	@Activate
+	public void activate(){
+		instance = this;
+	}
+	
+	@Deactivate
+	public void deactivate(){
+		instance = null;
+	}
+	
+	@Override
+	public void onEvent(org.dyamand.event.Event event) {
+		System.out.println("EVENT "+event.toString());
+	}
+
+	@Reference
+	void setEventAdmin(EventAdmin ea){
+		this.ea = ea;
+	}
 }
---
+++ be.iminds.iot.things.dyamand.adapter/src/be/iminds/iot/things/dyamand/adapter/DyamandAdapterPlugin.java
@@ -0,0 +1,25 @@
+package be.iminds.iot.things.dyamand.adapter;
+
+import java.util.Collections;
+
+import org.dyamand.DyamandException;
+import org.dyamand.plugin.Plugin;
+import org.dyamand.plugin.PluginContext;
+
+public class DyamandAdapterPlugin implements Plugin {
+
+	private PluginContext pluginContext;
+	
+	@Override
+	public void start(PluginContext context) throws DyamandException {
+		pluginContext = context;
+		// TODO what if DyamandAdapter not yet activated - can this happen?
+		context.register(DyamandAdapter.instance, Collections.EMPTY_LIST);
+	}
+
+	@Override
+	public void stop() throws DyamandException {
+		this.pluginContext.unregister(DyamandAdapter.instance);
+	}
+
+}
