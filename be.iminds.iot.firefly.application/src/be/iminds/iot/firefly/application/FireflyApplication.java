7e0812d5b0ae923788909a2b37ab36fc19372670
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-06-07 13:32:34 +0200

    use jsonrpc and eneasse in the firefly application, using my new enroute webresource annotations

--- be.iminds.iot.firefly.application/src/be/iminds/iot/firefly/application/FireflyApplication.java
+++ be.iminds.iot.firefly.application/src/be/iminds/iot/firefly/application/FireflyApplication.java
@@ -6,22 +6,20 @@ import osgi.enroute.capabilities.AngularUIWebResource;
 import osgi.enroute.capabilities.AngularWebResource;
 import osgi.enroute.capabilities.BootstrapWebResource;
 import osgi.enroute.capabilities.ConfigurerExtender;
+import osgi.enroute.capabilities.EasseWebResource;
 import osgi.enroute.capabilities.EventAdminSSEEndpoint;
+import osgi.enroute.capabilities.JsonrpcWebResource;
 import osgi.enroute.capabilities.WebServerExtender;
-import osgi.enroute.rest.api.REST;
-import osgi.enroute.rest.api.RESTRequest;
 
 @AngularWebResource(resource={"angular.js","angular-resource.js", "angular-route.js"}, priority=1000)
 @AngularUIWebResource(resource="ui-bootstrap-tpls.js")
 @BootstrapWebResource(resource="css/bootstrap.css")
+@EasseWebResource(resource={"easse.js","polyfill/eventsource.js"})
 @EventAdminSSEEndpoint
+@JsonrpcWebResource(resource={"jsonrpc.js"})
 @WebServerExtender
 @ConfigurerExtender
 @Component(name="be.iminds.iot.firefly")
-public class FireflyApplication implements REST {
-
-	public String getUpper(RESTRequest rq, String string) {
-		return string.toUpperCase();
-	}
+public class FireflyApplication {
 
 }
