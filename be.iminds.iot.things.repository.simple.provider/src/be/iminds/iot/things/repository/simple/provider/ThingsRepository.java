3ffa36a0a4bb81dcfc78254092e44621bad778af
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-06-26 10:28:37 +0200

    Read/write things from/to file

--- be.iminds.iot.things.repository.simple.provider/src/be/iminds/iot/things/repository/simple/provider/ThingsRepository.java
+++ be.iminds.iot.things.repository.simple.provider/src/be/iminds/iot/things/repository/simple/provider/ThingsRepository.java
@@ -1,6 +1,8 @@
 package be.iminds.iot.things.repository.simple.provider;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -8,38 +10,45 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
 
+import aQute.lib.json.JSONCodec;
+import aQute.lib.converter.TypeReference;
+import be.iminds.iot.things.api.Thing;
 import be.iminds.iot.things.repository.api.Repository;
 import be.iminds.iot.things.repository.api.ThingDTO;
 
 /**
  * 
  */
-@Component
-public class ThingsRepository implements Repository {
-
+@Component(property={"event.topics=be/iminds/iot/thing/*"})
+public class ThingsRepository implements Repository, EventHandler {
+	
+	private final static JSONCodec json = new JSONCodec();
+	
 	private Map<UUID, ThingDTO> things = new HashMap<>();
+	
 	
 	@Activate
 	public void activate(BundleContext context){
-		// TODO read from file?
-		ThingDTO button = new ThingDTO();
-		button.device = "test device";
-		button.service = "button service";
-		button.id = UUID.nameUUIDFromBytes((button.device+button.service).getBytes());
-		button.gateway = UUID.fromString(context.getProperty(Constants.FRAMEWORK_UUID)); // get gateway id
-		
-		button.name = "Button 1";
-		button.type = "button";
-		button.location = "Kitchen";
-		
-		things.put(button.id, button);
-		
-		// 
-		
+		try {
+			things = (Map<UUID, ThingDTO>) json.dec().from(new File("things.txt")).get(new TypeReference<Map<UUID,ThingDTO>>(){});
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Deactivate
+	public void deactivate(){
+		try {
+			json.enc().indent("\t").to(new File("things.txt")).put(things).close();
+		} catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
@@ -50,7 +59,7 @@ public class ThingsRepository implements Repository {
 
 	@Override
 	public Collection<ThingDTO> getThings() {
-		System.out.println("LIST");
+		System.out.println("LIST "+things.values().size());
 		return Collections.unmodifiableCollection(new ArrayList(things.values()));
 	}
 
@@ -58,6 +67,26 @@ public class ThingsRepository implements Repository {
 	public void putThing(ThingDTO thing) {
 		System.out.println("PUT");
 		things.put(thing.id, thing);
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+		// TODO Auto-generated method stub
+		UUID id = (UUID) event.getProperty(Thing.ID);
+		if(!things.containsKey(id)){
+			ThingDTO thing = new ThingDTO();
+			thing.id = id;
+			thing.gateway = (UUID) event.getProperty(Thing.GATEWAY);
+			thing.device = (String) event.getProperty(Thing.DEVICE);
+			thing.service = (String) event.getProperty(Thing.SERVICE);
+			thing.type = (String) event.getProperty(Thing.TYPE);
+			thing.name = thing.service;
+			
+			things.put(id, thing);
+		}
+
+		// TODO keep all events
+		// TODO keep track of online/offline thing status
 	}
 
 }
